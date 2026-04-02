d10020fabd0491390547107f1b5d420bb79b74a8
Nikhil-Darji02 <nikhildarji02@gmail.com>
2026-04-02 17:12:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,3 @@
 import streamlit as st
-import os
 
-st.title("Debug Mode 🚀")
-
-st.write("Files in repo:")
-st.write(os.listdir())
-
-try:
-    import pandas as pd
-    st.write("Pandas loaded ✅")
-
-    # Try loading dataset
-    if "Medicalpremium.csv" in os.listdir():
-        df = pd.read_csv("Medicalpremium.csv")
-        st.write("CSV Loaded ✅")
-    else:
-        st.error("CSV NOT FOUND ❌")
-
-    # Try loading model
-    import joblib
-    if "model.pkl" in os.listdir():
-        model = joblib.load("model.pkl")
-        st.write("Model Loaded ✅")
-    else:
-        st.error("MODEL NOT FOUND ❌")
-
-except Exception as e:
-    st.error(f"ERROR: {e}")
+st.title("App is working ✅")
